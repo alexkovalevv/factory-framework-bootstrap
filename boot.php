8e549fc792edb258e876a3cb20f950176355192c
alex.kovalevv <alex.kovalevv@gmail.com>
2019-07-30 05:49:17 +0300

    Обновлена версия модуля. Добавлена константа с версией модуля.

--- boot.php
+++ boot.php
@@ -1,37 +1,36 @@
 <?php
-	/**
-	 * Factory Bootstrap
-	 *
-	 * @author Alex Kovalev <alex.kovalevv@gmail.com>
-	 * @copyright (c) 2018, Webcraftic Ltd
-	 *
-	 * @package factory-bootstrap
-	 * @since 1.0.0
-	 */
+/**
+ * Factory Bootstrap
+ *
+ * @author        Alex Kovalev <alex.kovalevv@gmail.com>
+ * @since         1.0.0
+ * @package       factory-bootstrap
+ * @copyright (c) 2018, Webcraftic Ltd
+ *
+ */
 
-	// Exit if accessed directly
-	if( !defined('ABSPATH') ) {
-		exit;
-	}
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-	// module provides function only for the admin area
-	if( !is_admin() ) {
-		return;
-	}
+// module provides function only for the admin area
+if ( ! is_admin() ) {
+	return;
+}
 
-	if( defined('FACTORY_BOOTSTRAP_000_LOADED') ) {
-		return;
-	}
-	define('FACTORY_BOOTSTRAP_000_LOADED', true);
+if ( defined( 'FACTORY_BOOTSTRAP_000_LOADED' ) ) {
+	return;
+}
 
-	define('FACTORY_BOOTSTRAP_000_DIR', dirname(__FILE__));
-	define('FACTORY_BOOTSTRAP_000_URL', plugins_url(null, __FILE__));
+define( 'FACTORY_BOOTSTRAP_000_VERSION', '4.1.5' );
+define( 'FACTORY_BOOTSTRAP_000_LOADED', true );
 
-	// sets version of admin interface
-	define('FACTORY_BOOTSTRAP_000_VERSION', 'FACTORY_BOOTSTRAP_000');
+define( 'FACTORY_BOOTSTRAP_000_DIR', dirname( __FILE__ ) );
+define( 'FACTORY_BOOTSTRAP_000_URL', plugins_url( null, __FILE__ ) );
 
-	if( !defined('FACTORY_FLAT_ADMIN') ) {
-		define('FACTORY_FLAT_ADMIN', true);
-	}
+if ( ! defined( 'FACTORY_FLAT_ADMIN' ) ) {
+	define( 'FACTORY_FLAT_ADMIN', true );
+}
 
-	include_once(FACTORY_BOOTSTRAP_000_DIR . '/includes/functions.php');
+include_once( FACTORY_BOOTSTRAP_000_DIR . '/includes/functions.php' );
